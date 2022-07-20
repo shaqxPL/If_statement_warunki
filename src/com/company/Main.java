2449ce87e81e7230a03bd7fcf73fd1dd10fdef1b
shaqxPL <tomasz198720@gmail.com>
2022-07-20 19:39:47 +0200

    if - else if - else

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,10 +9,16 @@ public class Main {
         System.out.println("Give your number: ");
         Scanner userInput = new Scanner(System.in);
         int number = userInput.nextInt();
-        if (number<100){
-            System.out.println("Your number is less than 100 ");
+        if (number < 100 && number >= 1) {
+            System.out.println("Your number is between 1 and 99");
+        } else if (number < 1000 && number >= 100) {
+            System.out.println("Your number is between 100 and 999");
+        } else if (number < 10000 && number >= 1000) {
+            System.out.println("Your number is between 1000 and 9999");
+        } else if (number < 100000 && number >= 10000) {
+            System.out.println("Your number is between 10000 and 99999");
         } else {
-            System.out.println("Your number is higher than 100");
+            System.out.println("Your number is not between 1 and 99999");
         }
 
     }
