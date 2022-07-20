9c8da37284a4763043c6b34dcadaab934ef907d9
shaqxPL <tomasz198720@gmail.com>
2022-07-20 19:56:41 +0200

    is - else if - else: is number negative, positive or 0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,16 +9,13 @@ public class Main {
         System.out.println("Give your number: ");
         Scanner userInput = new Scanner(System.in);
         int number = userInput.nextInt();
-        if (number < 100 && number >= 1) {
-            System.out.println("Your number is between 1 and 99");
-        } else if (number < 1000 && number >= 100) {
-            System.out.println("Your number is between 100 and 999");
-        } else if (number < 10000 && number >= 1000) {
-            System.out.println("Your number is between 1000 and 9999");
-        } else if (number < 100000 && number >= 10000) {
-            System.out.println("Your number is between 10000 and 99999");
+        if (number > 0) {
+            System.out.println("Your number is positive");
+        }
+        else if (number < 0) {
+            System.out.println("Your number is negative");
         } else {
-            System.out.println("Your number is not between 1 and 99999");
+            System.out.println("Your number is 0");
         }
 
     }
